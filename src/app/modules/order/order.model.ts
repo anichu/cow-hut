e57b5f5b202aa66416a00fcd7747c7aa34dee771
anichu <anis.cse5.bu@gmail.com>
2023-07-08 22:03:04 +0600

    added order model

---
+++ src/app/modules/order/order.model.ts
@@ -0,0 +1,19 @@
+import { Schema, model } from "mongoose";
+import { IOrder, IOrderModel } from "./order.interface";
+
+const OrderSchema = new Schema<IOrder, IOrderModel>({
+	cow: {
+		type: Schema.Types.ObjectId,
+		ref: "Cow",
+		required: true,
+	},
+	buyer: {
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true,
+	},
+});
+
+const Order = model<IOrder, IOrderModel>("Order", OrderSchema);
+
+export default Order;
